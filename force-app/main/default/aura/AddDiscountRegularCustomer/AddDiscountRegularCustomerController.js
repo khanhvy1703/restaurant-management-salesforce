488b734f17f0bcd3fa51ed867f20e2243c9c2a12
Victoria Le <victoria.le1703@gmail.com>
2023-10-16 03:47:44 -0400

    add aura

---
+++ force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerController.js
@@ -0,0 +1,13 @@
+({
+    doInit : function(component, event, helper) {
+
+    },
+
+    add: function (component, event, helper) {
+
+    },
+
+    remove: function (component, event, helper) {
+
+    },
+})
