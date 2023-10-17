8823e09f954a85b4c39a793486a1c02fa321ac99
Victoria Le <victoria.le1703@gmail.com>
2023-10-17 10:27:12 -0400

    commit

--- force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerController.js
+++ force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerController.js
@@ -1,13 +1,13 @@
 ({
-    doInit : function(component, event, helper) {
+  doInit: function (component, event, helper) {
+    helper.doInit(component, event, helper);
+  },
 
-    },
+  add: function (component, event, helper) {
+    helper.add(component, event, helper);
+  },
 
-    add: function (component, event, helper) {
-
-    },
-
-    remove: function (component, event, helper) {
-
-    },
+  remove: function (component, event, helper) {
+    helper.remove(component, event, helper);
+  },
 })
--- force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerHelper.js
+++ force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerHelper.js
@@ -1,5 +1,44 @@
 ({
-    helperMethod : function() {
+	doInit: function (component, event, helper) {
+		var action = component.get("c.checkDiscountApex");
 
-    }
+		action.setCallback(this, function (a) {
+			console.log(a);
+			if (a.getState() === "SUCCESS") {
+				console.log(a.getReturnValue());
+				component.set("v.addDiscount", a.getReturnValue());
+				component.set("v.removeDiscount", !a.getReturnValue());
+			}
+		});
+
+		$A.enqueueAction(action);
+	},
+
+	add: function (component, event, helper) {
+		var action = component.get("c.addRegularDiscountApex");
+
+		action.setCallback(this, function (a) {
+			console.log(a);
+			if (a.getState() === "SUCCESS") {
+				component.set("v.addDiscount", true);
+				component.set("v.removeDiscount", false);
+			}
+		});
+
+		$A.enqueueAction(action);
+	},
+
+	remove: function (component, event, helper) {
+		var action = component.get("c.removeRegularDiscountApex");
+
+		action.setCallback(this, function (a) {
+			console.log(a);
+			if (a.getState() === "SUCCESS") {
+				component.set("v.addDiscount", false);
+				component.set("v.removeDiscount", true);
+			}
+		});
+
+		$A.enqueueAction(action);
+	}
 })
