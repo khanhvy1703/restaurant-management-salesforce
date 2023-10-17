7a32fd67ab7a97384339457e5f251a731f99630f
Ngoc Le (Victoria) <58549806+khanhvy1703@users.noreply.github.com>
2023-10-17 10:40:39 -0400

    add aura (#34)
    Add Discount Regular aura

---
+++ force-app/main/default/aura/AddDiscountRegularCustomer/AddDiscountRegularCustomerController.js
@@ -0,0 +1,13 @@
+({
+  doInit: function (component, event, helper) {
+    helper.doInit(component, event, helper);
+  },
+
+  add: function (component, event, helper) {
+    helper.add(component, event, helper);
+  },
+
+  remove: function (component, event, helper) {
+    helper.remove(component, event, helper);
+  },
+})
