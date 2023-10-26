342e7ff61879450cb2f03508a93ef2e91df44b0e
Victoria Le <victoria.le1703@gmail.com>
2023-10-26 01:22:17 -0400

    approve process

--- force-app/main/default/aura/MoveSalesStatus/MoveSalesStatusController.js
+++ force-app/main/default/aura/MoveSalesStatus/MoveSalesStatusController.js
@@ -1,9 +1,14 @@
 ({
 	doInit: function (component, event, helper) { 
 		helper.getOpp(component);
+		helper.cancelStatus(component);
 	},
 
 	change: function (component, event, helper) {
 		helper.changeStatus(component, event, helper);
+	},
+
+	cancel: function (component, event, helper) {
+		helper.cancelOrder(component, event, helper);
 	}
 })
--- force-app/main/default/aura/MoveSalesStatus/MoveSalesStatusHelper.js
+++ force-app/main/default/aura/MoveSalesStatus/MoveSalesStatusHelper.js
@@ -40,6 +40,41 @@
 		$A.enqueueAction(action);
 	},
 
+	cancelOrder: function (component, event, helper) {
+		var action = component.get("c.cancelOrderApex");
+
+		action.setParams({
+			oppId: component.get("v.recordId")
+		});
+
+		action.setCallback(this, function (a) {
+			if (a.getState() === "SUCCESS") {
+				helper.showToast(component, event, helper, "success", "Canceled", "Order is canceled. Waiting for manager approval.");
+				$A.get('e.force:refreshView').fire();
+			} else {
+				helper.showToast(component, event, helper, "error", "ERROR", "Can not cancel order.");
+			}
+		});
+
+		$A.enqueueAction(action);
+	},
+
+	cancelStatus: function (component) {
+		var action = component.get("c.getCancelStatus");
+
+		action.setParams({
+			oppId: component.get("v.recordId")
+		});
+
+		action.setCallback(this, function (a) {
+			if (a.getState() === "SUCCESS") {
+				component.set("v.btn", a.getReturnValue());
+			}
+		});
+
+		$A.enqueueAction(action);
+	},
+
 	showToast: function (component, event, helper, toastType, title, message) {
 		var toastEvent = $A.get("e.force:showToast");
 		toastEvent.setParams({
