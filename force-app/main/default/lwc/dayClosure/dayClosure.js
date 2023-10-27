87765279786183a10053c24b87ed3d183db8119c
Victoria Le <victoria.le1703@gmail.com>
2023-10-27 18:40:13 -0400

    catfact + dayclosure

---
+++ force-app/main/default/lwc/dayClosure/dayClosure.js
@@ -0,0 +1,15 @@
+import { LightningElement, wire } from 'lwc';
+import getSalesInfo from '@salesforce/apex/DayClosureController.getSalesInfo';
+
+export default class DayClosureLWC extends LightningElement {
+  salesInfo;
+
+  @wire(getSalesInfo)
+  wiredSalesInfo({ error, data }) {
+    if (data) {
+      this.salesInfo = data;
+    } else if (error) {
+      console.log('Error loading sales info: ' + error);
+    }
+  }
+}
